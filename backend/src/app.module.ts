1f803a3b74b9c41bb25c06ee6bc8e24aaa922a74
g-30 <8287493+g-30@users.noreply.github.com>
2023-03-25 20:55:17 +0600

    main module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { VideoModule } from './video/video.module';
+import { WaitlistModule } from './waitlist/waitlist.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      // this is publicly exposed but that's fine at this stage of dev
+      username: 'root',
+      password: 'test',
+      database: 'education_portal',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // remove after db is set up
+      synchronize: true,
+    }),
+    UserModule,
+    CourseModule,
+    VideoModule,
+    WaitlistModule,
+  ],
 })
 export class AppModule {}
