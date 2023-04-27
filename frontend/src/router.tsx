c28bb77ba9fd6f368e3ccdac472ffbc726f17f98
muratk <kupenovmurat@gmail.com>
2023-04-28 02:06:23 +0600

    fixes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import Login from './pages/Login/Login'
 import Registration from './pages/Registration/Registration'
-import HomePage from './pages/Home/Home'
+import Home from './pages/Home/Home'
 import Courses from './pages/Courses/Courses'
 
 export const router = createBrowserRouter([
@@ -21,7 +21,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/',
-                element: <HomePage />,
+                element: <Home />,
             },
             {
                 path: '/courses',
