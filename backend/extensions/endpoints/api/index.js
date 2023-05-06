5acd8da0e19f77efb7a5c58953b0c09e67120510
g-30 <8287493+g-30@users.noreply.github.com>
2023-05-06 10:03:44 +0000

    videos paywall

--- backend/extensions/endpoints/api/index.js
+++ backend/extensions/endpoints/api/index.js
@@ -15,23 +15,23 @@ export default (router, { services, exceptions }) => {
 
 	router.post('/register', async (req, res, next) => {
 		if (req.body.email == null || req.body.password == null) {
-			return res.status(400).json({ status: 400, status_text: 'Please provide email & password fields.' });
+			return res.status(400).json({ status: 0, status_text: 'Please provide email & password fields.' });
 		}
 		const email = `${req.body.email}`.toLowerCase();
 		if (!/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi.test(email)) {
-			return res.status(400).json({ status: 400, status_text: 'Email field is invalid.' });
+			return res.status(400).json({ status: 0, status_text: 'Email field is invalid.' });
 		}
 		if (`${req.body.first_name || ''}`.length < 2) {
-			return res.status(400).json({ status: 400, status_text: 'First name field has to be 2 or more characters long.' });
+			return res.status(400).json({ status: 0, status_text: 'First name field has to be 2 or more characters long.' });
 		}
 		if (`${req.body.last_name || ''}`.length < 2) {
-			return res.status(400).json({ status: 400, status_text: 'Last name field has to be 2 or more characters long.' });
+			return res.status(400).json({ status: 0, status_text: 'Last name field has to be 2 or more characters long.' });
 		}
 		if (`${req.body.phone || ''}`.length < 11) {
-			return res.status(400).json({ status: 400, status_text: 'Phone field has to be 11 or more characters long.' });
+			return res.status(400).json({ status: 0, status_text: 'Phone field has to be 11 or more characters long.' });
 		}
 		if (`${req.body.password}`.length < passMinLength) {
-			return res.status(400).json({ status: 400, status_text: `Password should be ${passMinLength} or more characters long.` });
+			return res.status(400).json({ status: 0, status_text: `Password should be ${passMinLength} or more characters long.` });
 		}
 
 		const usersService = new UsersService({ schema: req.schema });
@@ -49,17 +49,17 @@ export default (router, { services, exceptions }) => {
 			// await usersService.inviteUrl(email, studentRoleId, null);
 			await usersService.createOne(payload);
 		} catch (err) {
-			return res.status(500).json({ status: 500, status_text: err.toString() });
+			return res.status(500).json({ status: 0, status_text: err.toString() });
 		}
 
 		let authData;
 		try {
 			authData = await authService.login('default', payload);
 		} catch (err) {
-			return res.status(500).json({ status: 500, status_text: err.toString() });
+			return res.status(500).json({ status: 0, status_text: err.toString() });
 		}
 
-		return res.status(201).json({ status: 201, access_token: authData.accessToken, expires: authData.expires });
+		return res.status(201).json({ status: 1, access_token: authData.accessToken, expires: authData.expires });
 	});
 
 	router.get('/courses', (req, res, next) => {
@@ -70,7 +70,7 @@ export default (router, { services, exceptions }) => {
 		coursesService
 			.readByQuery({ sort: ['id'], fields: ['*'] })
 			.then((results) => res.json({
-				status: 200,
+				status: 1,
 				results: results.map((row) => parseCourseData(row)),
 			}))
 			.catch((error) => {
@@ -91,7 +91,7 @@ export default (router, { services, exceptions }) => {
 		}
 
 		if (courseData == null) {
-			return res.json({ status: 404 });
+			return res.status(404).json({ status: 0 });
 		}
 
 		let paymentStatus;
@@ -120,7 +120,7 @@ export default (router, { services, exceptions }) => {
 		}
 
 		res.json({
-			status: 200,
+			status: 1,
 			result: {
 				...parseCourseData(courseData),
 				payment: {
@@ -143,14 +143,39 @@ export default (router, { services, exceptions }) => {
 			return next(new ServiceUnavailableException(error.message));
 		}
 
-		if (videoData == null) {
-			return res.json({ status: 404 });
+		if (videoData == null || videoData.status !== 'published') {
+			return res.status(404).json({ status: 0 });
+		}
+		const courseId = videoData['course_id'];
+
+		let paymentStatus;
+		if (req.accountability != null && req.accountability.user != null) {
+			let paymentData;
+			try {
+				paymentData = await coursePaymentForUserUuid(courseId, req.accountability.user, req);
+			} catch (error) {
+				return next(new ServiceUnavailableException(error.message));
+			}
+			if (paymentData != null) {
+				paymentStatus = paymentData.status;
+			}
+		}
+
+		if (paymentStatus == null || paymentStatus !== 'paid') {
+			return res.status(402).json({
+				status: 0,
+				status_text: 'Payment required',
+				payment: {
+					status: paymentStatus,
+				},
+			});
 		}
 
 		res.json({
-			status: 200,
+			status: 1,
 			result: {
 				...parseVideoData(videoData),
+				description: videoData['description'],
 				video_iframe_url: videoData['video_iframe_url'],
 			}
 		});
@@ -160,7 +185,7 @@ export default (router, { services, exceptions }) => {
 		/** @type {DirectusAccountability} */
 		const account = req.accountability;
 		if (account == null || account.user == null) {
-			return res.status(401).json({ status: 401, status_text: 'User has to be authorized.' });
+			return res.status(401).json({ status: 0, status_text: 'User has to be authorized.' });
 		}
 		const userUuid = account.user;
 
@@ -181,7 +206,7 @@ export default (router, { services, exceptions }) => {
 		}
 
 		if (paymentData != null && paymentData.status === 'paid') {
-			return res.status(500).json({ status: 500, status_text: 'Course is already paid' });
+			return res.status(500).json({ status: 0, status_text: 'Course is already paid' });
 		}
 		const priceToPay = computeCoursePayment(courseData, paymentData);
 		if (paymentData == null) {
@@ -198,7 +223,7 @@ export default (router, { services, exceptions }) => {
 
 		const httpStatus = paymentData == null ? 201 : 200;
 		res.status(httpStatus).json({
-			status: httpStatus,
+			status: 1,
 			result: {
 				price_to_pay: priceToPay,
 			},
